f7685b2deca9562e7c50ef6007d63610fdfc01d1
KathSanti <kath.santibanez@duocuc.cl>
2026-02-23 01:22:15 -0300

    commit

--- src/main/java/Speedfast/com/dao/ConexionBD.java
+++ src/main/java/Speedfast/com/dao/ConexionBD.java
@@ -8,7 +8,7 @@ public class ConexionBD {
 
     private static final String URL = "jdbc:mysql://localhost:3306/speedfast_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "Kth.20983Tft"; // Cambia por tu clave
+    private static final String PASSWORD = "ingresa tu clave"; // Cambia por tu clave
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
